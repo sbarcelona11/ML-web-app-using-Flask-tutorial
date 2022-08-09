e82f398732545c2988d7d925267c62ed3b898f41
Sebastian Barcelona <sbarcelona@gmail.com>
2022-08-09 03:18:58 +0000

    added changes

--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import pandas as pd
 
-# your code here
+app = Flask(__name__)
+model = pickle.load(open('./models/titanic_model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = []
+    for x in request.form.values():
+        features.append(x)
+    
+    cols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+    final_features = pd.DataFrame(np.array(features).reshape(1,7), columns=cols)
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Would you survive? {} (1=survived, 0=deceased)'.format(output))
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
+
+# FLASK_APP=/src/app.py
